9c814d59e0d0411357267c5d94252762085352d1
Orazbay Ualesh <oualesh@MacBook-Pro-Orazbay.local>
2022-06-17 12:55:45 +0600

    fingerprint added

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
-window.addEventListener("message", function messageHandler(e) {
-    try {
-        const data = JSON.parse(e.data);
+const idGetter = isSafari() ? getIdFromFingerprint : getIdFromIframe;
 
-        if (data.event != "getId") return
+idGetter().then(id => setAppId(id));
 
-        window.removeEventListener("message", messageHandler);
-        setAppId(data.value || createId());
-    } catch (e) {
-        console.error(e)
-    }
-})
 
-addIframe();
+function isSafari() { return /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification)); }
 
 function setAppId(id) {
     window.userId = id;
     alert(id)
 }
 
-function addIframe() {
-    var iframe = document.createElement('iframe');
-    iframe.style.display = "none";
-    iframe.src = 'https://orazbay097.github.io/test_iframe/'
-    document.body.appendChild(iframe);
+
+function getIdFromIframe() {
+    return new Promise((resolve, reject) => {
+        window.addEventListener("message", function messageHandler(e) {
+            try {
+                const data = JSON.parse(e.data);
+
+                if (data.event != "getId") return
+
+                window.removeEventListener("message", messageHandler);
+                resolve(data.value)
+            } catch (e) {
+                reject(e);
+            }
+        })
+
+        const iframe = document.createElement('iframe');
+        iframe.style.display = "none";
+        iframe.src = 'https://orazbay097.github.io/test_iframe/'
+        document.body.appendChild(iframe);
+    })
+}
+
+function getIdFromFingerprint() {
+    const KEY_USER_ID = 'fingerprint-userid';
+    const id = localStorage.getItem(KEY_USER_ID);
+
+    if (id) return Promise.resolve(id);
+
+    import('https://fpcdn.io/v3/1ItjaKwplSsZ3CpiS7pJ')
+        .then(FingerprintJS => FingerprintJS.load())
+        .then(fp => fp.get())
+        .then(result => {
+            localStorage.setItem(KEY_USER_ID, result.visitorId);
+            return result.visitorId
+        });
 }
